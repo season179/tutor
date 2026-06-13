7994bd7ba05e6ec9f6688a0aaf4569ede3a2f82e
Season <season.saw@gmail.com>
2026-06-13 19:40:59 +0800

    Add TypeScript support

---
+++ index.ts
@@ -0,0 +1,7 @@
+import { registerRootComponent } from 'expo';
+
+import App from './App';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App).
+// It also ensures the environment is set up for Expo Go and native builds.
+registerRootComponent(App);
